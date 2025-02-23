91e2ce34f39f4c5960060dd6183b4d871a04ed40
dattatele <datta.tele@gmail.com>
2025-02-23 00:17:11 -0600

    fix(artgen): removed commented script from cli.py

--- artgen/cli.py
+++ artgen/cli.py
@@ -9,28 +9,9 @@ from duckduckgo_search import DDGS
 from io import BytesIO
 from PIL import Image
 
-import sys
 import traceback
 import click
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
-ASCII_CHARS = ASCII_CHARS[::-1]
-
-new_width=100
-
-def get_ansi_color(value):
-    r, g, b = value
-    return '\x1b[38;2;{r};{g};{b}m'.format(r=str(r).zfill(3), g=str(g).zfill(3), b=str(b).zfill(3))
-
-def grayscalify(image):
-    return image.convert('L')
-
-def modify(image_g, image, buckets=25):
-    initial_pixels_g = list(image_g.getdata())
-    initial_pixels = list(image.getdata())
-
-    new_pixels = [get_ansi_color(initial_pixels[i]) + ASCII_CHARS[pixel_value//buckets] for i, pixel_value in enumerate(initial_pixels_g)]
-    return ''.join(new_pixels)
 
 def fetch_image_object(query):
     """
@@ -129,51 +110,6 @@ def generate_img_cmd(description):
             traceback.print_exc()
     else:
         click.echo("No image available for the query.")
-
-
-
-# def main():
-#     parser = argparse.ArgumentParser(
-#         description="ArtGen CLI: Generate ASCII art based on a brief description."
-#     )
-
-#     subparsers = parser.add_subparsers(dest="command", required=True)
-
-#     # Subcommand: generate_art
-#     parser_art = subparsers.add_parser("generate_art", help="Generate ASCII art from an image.")
-#     parser_art.add_argument("description", type=str, help="A brief description (preferably a single word)")
-
-#     # Subcommand: generate_word
-#     parser_word = subparsers.add_parser("generate_word", help="Generate stylized text using pyfiglet.")
-#     parser_word.add_argument("description", type=str, help="A brief description")
-
-#     # Subcommand: generate_img
-#     parser_img = subparsers.add_parser("generate_img", help="Fetch and save an image locally.")
-#     parser_img.add_argument("description", type=str, help="A brief description")
-
-#     # parser.add_argument(
-#     #     "command",
-#     #     choices=["generate_art", "generate_word", "generate_img"],
-#     #     help="Command to run: 'generate_art' for ASCII art, 'generate_word' for stylized text, 'generate_img' for image display."
-#     # )
-#     # parser.add_argument(
-#     #     "description",
-#     #     help="A brief description (preferably a single word for best results)"
-#     # )
-
-#     args = parser.parse_args()
-
-#     if args.command == "generate_art":
-#         generate_art(args.description)
-
-#     elif args.command == "generate_word":
-#         print(pyfiglet.figlet_format(args.description))
-
-#     elif args.command == "generate_img":
-#         print(generate_img(args.description))
-#     else:
-#         parser.print_help()
-#         sys.exit(1)
 
 if __name__ == "__main__":
     cli()
